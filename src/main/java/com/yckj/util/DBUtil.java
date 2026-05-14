7c564cb954cbf22fcb521af1b753c1d07d7313ae
wangjimgrui6-arch <wangjimgrui66@gmail.com>
2026-05-14 19:13:34 +0800

    Changes

--- src/main/java/com/yckj/util/DBUtil.java
+++ src/main/java/com/yckj/util/DBUtil.java
@@ -8,8 +8,7 @@ package com.yckj.util;
 import java.sql.*;
 
 /**
- * 数据库连接，获得数据库
- *
+ *数据库实用类，获得数据库连接和关闭资源，针对MySQL8
  * @author wjr
  * @version 1.0
  */
@@ -18,26 +17,30 @@ public class DBUtil {
         System.out.println(DBUtil.getConnection());
 
     }
+
     /**
-     * @return
+     * 获得数据库连接
+     * @return 数据库连接
      */
     public static Connection getConnection() {
         Connection conn = null;
         //注册MySQL驱动
         try {
+            //注册驱动程序，MySQL驱动
             Class.forName("com.mysql.cj.jdbc.Driver"); //全限定类名
-            String url = "jdbc:mysql://localhost:3306/java1036db?serverTimzone=UTC";
+            String url = "jdbc:mysql://localhost:3306/java1036db?serverTimezone=UTC";
             conn = DriverManager.getConnection(url, "root", "123456");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return conn;
     }
 
     /**
-     * 关闭资源
+     * 关闭资源（关闭数据库）
      *
      * @param rs    结果集
      * @param pstmt 预编译对象
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<%
+    for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+    }
+%>
 </body>
 </html>
